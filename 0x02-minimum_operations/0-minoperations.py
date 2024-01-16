ade72b6277a5e17463fd34f786d8f9007237241d
Zak-devz <zakariarassili@gmail.com>
2024-01-16 13:48:10 +0100

    changed the cod to shorter one

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,18 +4,20 @@
 
 
 def minOperations(n):
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    """ Minimum Operations needed to get n H characters
+        Args:
+            n: input value
+        Return: the sum of the operations """
+    if n < 2:
         return 0
-    return op
+
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+
+    return sum(factor_list)
